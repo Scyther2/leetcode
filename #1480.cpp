7ac8b0162098c5285c6ed0ca17e9920d73e44065
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-08-25 16:42:46 +0530

    Create #1480.cpp

---
+++ #1480.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n);
+        
+        ans[0]=nums[0];
+        
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=nums[i]+ans[i-1];
+        }
+        
+        return ans;
+    }
+};
