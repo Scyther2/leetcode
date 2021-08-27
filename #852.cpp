b30ce961f2eccd7ccc9516bae2e6615741824c94
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-08-27 09:55:46 +0530

    Create #852.cpp

---
+++ #852.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        
+        int l=0;
+        int r=n-1;
+        
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            
+            if(mid==0)
+            {
+                return 1;
+            }
+            else if(mid==n-1)
+            {
+                return n-2;
+            }
+            else if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        
+        return 0;
+    }
+};
