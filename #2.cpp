a01b15d35788b5003480e6414ee85dec129a1430
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-09-11 11:32:20 +0530

    Create #2.cpp

---
+++ #2.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans=new ListNode(0);
+        ListNode* head=ans;
+        int sum=0;
+        
+        while(sum || l1|| l2)
+        {
+            sum+=(l1?l1->val:0)+(l2?l2->val:0);
+            ListNode* temp=new ListNode(sum%10);
+            ans->next=temp;
+            ans=ans->next;
+            sum/=10;
+            if(l1!=NULL)
+            {
+                l1=l1->next;
+            }
+            if(l2!=NULL)
+            {
+                l2=l2->next;
+            }
+        }
+        
+        return head->next;
+    }
+};
