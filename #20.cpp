ef3586187d0252d4d5770786ac062644dfdb53c5
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-09-20 10:07:58 +0530

    Create #20.cpp

---
+++ #20.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+            {
+                stk.push(s[i]);
+            }
+            else
+            {
+                if(stk.empty())
+                {
+                    return false;
+                }
+                if(s[i]==')')
+                {
+                    if(stk.top()=='(')
+                    {
+                        stk.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if(s[i]==']')
+                {
+                    if(stk.top()=='[')
+                    {
+                        stk.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if(s[i]=='}')
+                {
+                    if(stk.top()=='{')
+                    {
+                        stk.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        if(stk.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
