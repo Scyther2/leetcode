40b95cd7131d10292c8c53ae5628ad1dee472dca
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-09-21 11:13:52 +0530

    Create #1021.cpp

---
+++ #1021.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.size();
+        int count=0;
+        string ans="";
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                if(count++)
+                {
+                    ans+=s[i];
+                }
+            }
+            else
+            {
+                if(--count)
+                {
+                    ans+=s[i];
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
