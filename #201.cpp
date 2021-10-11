ea9c8c875bac63340cc5e5a0c3980bae1361c8ca
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-10-11 19:17:30 +0530

    Create #201.cpp

---
+++ #201.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        while(right>left)
+        {
+            right&=right-1;
+        }
+        
+        
+        return right;
+        
+    }
+};
