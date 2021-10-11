54bb2d64c2579e58ae61741265109135797ac70b
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-10-11 19:16:25 +0530

    Create #189(in-place).cpp

---
+++ #189(in-place).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        reverse(nums.begin(),nums.begin()+n-(k%n));
+        
+        reverse(nums.begin()+n-(k%n),nums.end());
+        
+        reverse(nums.begin(),nums.end());
+        
+        
+        
+        
+    }
+};
