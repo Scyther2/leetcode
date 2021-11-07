3e649d7be2be73605628e61dd4283b998bfca2b3
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-11-07 10:45:21 +0530

    Create #144.cpp

---
+++ #144.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void trv(TreeNode* root,vector<int>&ans)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        
+        ans.push_back(root->val);
+        trv(root->left,ans);
+        
+        
+        
+        trv(root->right,ans);
+        
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        trv(root,ans);
+        
+        return ans;
+        
+    }
+};
