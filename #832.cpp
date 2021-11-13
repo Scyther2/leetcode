828b5e1e9daccaaf7f3e4411ff40dd8a744cd61d
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-11-13 15:17:32 +0530

    Create #832.cpp

---
+++ #832.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        for(int i=0;i<image.size();i++)
+        {
+            reverse(image[i].begin(),image[i].end());
+        }
+        
+        for(int i=0;i<image.size();i++)
+        {
+            for(int j=0;j<image[0].size();j++)
+            {
+                if(image[i][j]==0)
+                {
+                    image[i][j]=1;
+                    
+                }
+                else
+                {
+                    image[i][j]=0;
+                }
+            }
+        }
+        
+        return image;
+    }
+};
