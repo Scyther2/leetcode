4c3e3bb86874961918498ac3cd98b9e44b5e9259
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-11-23 07:26:37 +0530

    Create #1845.cpp

---
+++ #1845.cpp
@@ -0,0 +1,33 @@
+class SeatManager {
+private:
+    priority_queue<int,vector<int>,greater<int>>q;
+    
+public:
+    SeatManager(int n) {
+        
+        for(int i=1;i<n+1;i++)
+        {
+            q.push(i);
+        }
+    }
+    
+    int reserve() {
+        int x=q.top();
+        q.pop();
+        
+        
+        return x;
+    }
+    
+    void unreserve(int seatNumber) {
+        
+        q.push(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
