6921edca8dac7c2caa8b54e87f18058dc22faf18
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2021-11-27 20:07:50 +0530

    Create #1486.cpp

---
+++ #1486.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int>v(n);
+        int ans=v[0];
+        
+        for(int i=0;i<n;i++)
+        {
+            v[i]=start+(2*i);
+        }
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            ans=ans^v[i];
+        }
+        
+        return ans;
+    }
+};
