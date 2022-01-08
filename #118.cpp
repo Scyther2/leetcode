20555ec692797382c1b200c80c1813838763965a
Nihar Thummar <83320793+Scyther2@users.noreply.github.com>
2022-01-08 10:36:59 +0530

    Create #118.cpp

---
+++ #118.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        ans.push_back({1});
+        
+        for(int i=1;i<numRows;i++)
+        {
+            vector<int>temp;
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0)
+                {
+                    temp.push_back(ans[i-1][j]);
+                }
+                else if(j==i)
+                {
+                    temp.push_back(ans[i-1][j-1]);
+                }
+                else 
+                {
+                    temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        
+        return ans;
+    }
+};
